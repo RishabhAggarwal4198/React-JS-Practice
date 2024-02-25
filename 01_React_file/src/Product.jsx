a8ecf29c6c11b7aaf2ddb7b554469ef14ad4cafc
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-25 17:28:44 +0530

    Changes

--- 01_React_file/src/Product.jsx
+++ 01_React_file/src/Product.jsx
@@ -4,9 +4,9 @@ import "./Product.css"
 
 function Product ({name,age, state}) {
 	return (
-		<div>
+		<div className="div1">
 		<h1> Hello My Name is {name} </h1>
-		<h2>  My Age is {age} </h2>
+		<h2> My Age is {age} </h2>
 		<h3> i belongs to the Indian state of  "{state}"</h3>
 		</div>
 	);
