1bc846068a959ab7dc61ec22d8d3f83e4e5a0fa5
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-25 17:55:25 +0530

    Changes

--- 01_React_file/src/ProductGroup.jsx
+++ 01_React_file/src/ProductGroup.jsx
@@ -5,7 +5,8 @@ function ProductGroup (){
 	return (
 		<>
 		<Product name="Ramesh" age="22"    state="Rajasthan" />
-		<Product name="Suresh" age = "55"  state="Tamil Nadu" />
+		<Product name="Vimal" age="44"    state="Haryana" />
+		<Product name="Suresh" age = "55"  state="Tamil Nadu"/>
 		</>
 	)
 }
