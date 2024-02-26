c6665f0a0ad132fe8cc56096180a38bce440d732
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-26 13:59:56 +0530

    Changes

--- 01_React_file/src/Product.jsx
+++ 01_React_file/src/Product.jsx
@@ -3,14 +3,14 @@ import "./Product.css"
 
 
 function Product ({name, age, state, year = `"Not mentioned"` , sequence}) {
-	const numCount = num.map( (n) => <li>{n}</li>)
+	// const numCount = num.map( (n) => <li>{n}</li>)
 	return (
 		<div className="div1">
 		<h1> Hello My Name is {name} </h1>
 		<h2> My Age is : {age} </h2>
 		<h3> I belongs to the Indian state of  "{state}"</h3>
 		<h4> I did my graduation in : {year} </h4>
-		<p> Tag : {numCount}</p>
+		<p> Tag : {num}</p>
 		</div>
 	);
 }
