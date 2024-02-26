c876cc09720dcd192ba81c4538afc4c9b727f250
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-26 14:12:14 +0530

    Changes

--- 01_React_file/src/Product.jsx
+++ 01_React_file/src/Product.jsx
@@ -2,7 +2,7 @@
 import "./Product.css"
 
 
-function Product ({name, age, state, year = `"Not mentioned"` , sequence , obj2}) {
+function Product ({name, age, state, year = `"Not mentioned"` , sequence , objTwo}) {
 	// const numCount = num.map( (n) => <li>{n}</li>)
 	return (
 		<div className="div1">
@@ -11,7 +11,7 @@ function Product ({name, age, state, year = `"Not mentioned"` , sequence , obj2}
 		<h3> I belongs to the Indian state of  "{state}"</h3>
 		<h4> I did my graduation in : {year} </h4>
 		<p> My Sequence is : {sequence}</p>
-		<p> My Object is : {obj2}</p>
+		<p> My Object is : {objTwo.A}</p>
 		</div>
 	);
 }
--- 01_React_file/src/ProductGroup.jsx
+++ 01_React_file/src/ProductGroup.jsx
@@ -7,7 +7,7 @@ function ProductGroup (){
 	let num2 =  {A : 'Pratham', B : 'Dwitya', C : 'Tritya', D: 'Chaturtha' }
 	return (
 		<div className="div2">
-		<Product name="Ramesh" age= {22}   state="Rajasthan"  sequence = {num}  obj2 = {num2} />
+		<Product name="Ramesh" age= {22}   state="Rajasthan"  sequence = {num}  objTwo = {num2} />
 		<Product name="Vimal" age= {44}    state="Haryana" />
 		<Product name="Suresh" age = {55/2}  state="Tamil Nadu"/>
 		</div>
