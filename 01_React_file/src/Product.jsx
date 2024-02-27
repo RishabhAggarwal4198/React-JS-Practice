3dfd689dfe5637892218a6bd6af7c698926e3776
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 11:05:08 +0530

    Changes

--- 01_React_file/src/Product.jsx
+++ 01_React_file/src/Product.jsx
@@ -3,15 +3,16 @@ import "./Product.css"
 
 
 function Product ({name, age, state, year = `"Not mentioned"` , sequence , objTwo }) {
+	const numCount = sequence.map( (ni) => <li>{ni}</li> )
 	return (
 		<div className="div1">
 		<h1> Hello My Name is {name} </h1>
 		<h2> My Age is : {age} </h2>
 		<h3> I belongs to the Indian state of  "{state}"</h3>
 		<h4> I did my graduation in : {year} </h4>
-		<p> My Sequence is : {sequence}</p>
+		<p> My Sequence is : {numCount}</p>
 		<p> My Object is : {objTwo}</p> 
-		{/* <p> My Object is : {objTwo.a}</p>  */}
+		{/* <p> My Object is : {objTwo.a}</p>  */} 
 		</div>
 	);
 }
--- 01_React_file/src/ProductGroup.jsx
+++ 01_React_file/src/ProductGroup.jsx
@@ -3,13 +3,13 @@ import "./ProductGroup.css"
 import { Product } from "./Product.jsx";
 
 function ProductGroup (){
-	const num = ['Pratham', 'Dwitya', 'Tritya', 'Chaturth']
+	const nums = ['Pratham', 'Dwitya', 'Tritya', 'Chaturth']
 	let num2 =  { pehla : 'Pratham', dudra : 'Dwitya', teesra : 'Tritya', chautha: 'Chaturtha' }
-	// const numCount = num.map( (n) => <li>{n}</li>)
+	
 
 	return (
 		<div className="div2">
-		<Product name="Ramesh" age= {22}   state="Rajasthan"  sequence = {num}  objTwo = {['Hello', 'World']} />
+		<Product name="Ramesh" age= {22}   state="Rajasthan"  sequence = {nums}  objTwo = {['Hello', 'World']} />
 		{/* <Product name="Ramesh" age= {22}   state="Rajasthan"  sequence = {num}  objTwo = { {'a': 'Hello', 'b': 'World'}} /> */}
 		<Product name="Vimal" age= {44}    state="Haryana"  sequence = {num}  />
 		<Product name="Suresh" age = {55/2}  state="Tamil Nadu"  sequence = {num}  />
