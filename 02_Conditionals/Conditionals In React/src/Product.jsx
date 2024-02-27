d92f7be594d448586c167106473d692ccefc4371
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 15:59:19 +0530

    Changes

--- 02_Conditionals/Conditionals In React/src/Product.jsx
+++ 02_Conditionals/Conditionals In React/src/Product.jsx
@@ -4,7 +4,7 @@ import "./Product.css"
 
 function Product ({name, age, state, year = `"Not mentioned"`}) {
 
-	let gradYear = year > 2005 ? "You exceed the GRADUATION-YEAR limit " : "Your GRADUATION-YEAR is fine"
+	let gradYear = year > 2005 ? "❌You exceed the GRADUATION-YEAR limit❌" : " ✅Your GRADUATION-YEAR is fine✅"
 	let ageCriteria = age > 30 ? "You are ELIGIBLE " : "You are NOT-ELIGIBLE "
 
 	return (
@@ -12,7 +12,7 @@ function Product ({name, age, state, year = `"Not mentioned"`}) {
 		<h1> Hello My Name is {name} </h1>
 		<h2> My Age is : {age} </h2>
 		<h3> I belongs to the Indian state of  "{state}"</h3>
-		<h4> Graduation year: {year} </h4>
+		<h4> Graduation year: <b>{gradYear}</b> </h4>
 
 					{/* OR 🔽🔽 */}
 					
