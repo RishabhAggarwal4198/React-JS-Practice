085d6b659b3a1c8099ddeeafe4b03bdd7785a3df
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 15:48:34 +0530

    Changes

--- 02_Conditionals/Conditionals In React/src/Product.jsx
+++ 02_Conditionals/Conditionals In React/src/Product.jsx
@@ -4,7 +4,7 @@ import "./Product.css"
 
 function Product ({name, age, state, year = `"Not mentioned"`}) {
 
-	let filter = age > 30 ? "You are ELIGIBLE for this post" : "You are NOT-ELIGIBLE for this post"
+	let filter = age > 30 ? "You are ELIGIBLE " : "You are NOT-ELIGIBLE "
 
 	return (
 		<div className="div1">
@@ -12,6 +12,7 @@ function Product ({name, age, state, year = `"Not mentioned"`}) {
 		<h2> My Age is : {age} </h2>
 		<h3> I belongs to the Indian state of  "{state}"</h3>
 		<h4> I did my graduation in year : {year} </h4>
+		<h4> Eligibility : {filter} </h4>
 		</div>
 	);
 }
