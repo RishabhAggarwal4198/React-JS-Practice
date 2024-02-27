763df256d2e9c256f745c462c50f74b124587286
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 16:18:09 +0530

    Changes

--- 02_Conditionals/Conditionals In React/src/Product.jsx
+++ 02_Conditionals/Conditionals In React/src/Product.jsx
@@ -12,11 +12,13 @@ function Product ({name, age, state, year = `"Not mentioned"`}) {
 		<h2> My Age is : {age} </h2>
 		<h3> I belongs to the Indian state of  "{state}"</h3>
 		<h4> Graduation year: <b>{gradYear}</b> </h4>
-
-					{/* OR 🔽🔽 */}
-					
+					{/* OR 🔽🔽 */}	
 		<h4> Eligibility : {year < 2005 ? "You are ELIGIBLE " : "You are NOT-ELIGIBLE "} </h4>
-		<b> Age Factor :</b>  {age <= 30 ? <h4>You are UNDER-AGE for this post</h4> :  <h4> Your AGE is fine</h4>  }
+		
+
+		{/* <b> Age Factor :</b> {age <= 30 ? <h4>You are UNDER-AGE for this post</h4> :  <h4> Your AGE is fine</h4>  } */}
+									{/* OR 🔽🔽 */}
+	 {age <= 30 &&   <b> Age Factor : <h4>You are UNDER-AGE for this post</h4></b> }
 
 		</div>
 	);
