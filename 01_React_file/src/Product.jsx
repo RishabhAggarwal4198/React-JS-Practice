1343f742512e610a678a6db0ecee45ca43b0a609
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 11:24:55 +0530

    Changes

--- 01_React_file/src/Product.jsx
+++ 01_React_file/src/Product.jsx
@@ -3,7 +3,7 @@ import "./Product.css"
 
 
 function Product ({name, age, state, year = `"Not mentioned"` , sequence , objTwo , features }) {
-	// const numCount = sequence.map( (seq) => <li>{seq}</li> )
+	const numCount = sequence.map( (seq) => <li>{seq}</li> )
 	const list = features.map((feat) => <li>{feat}</li>)
 
 	return (
@@ -12,9 +12,9 @@ function Product ({name, age, state, year = `"Not mentioned"` , sequence , objTw
 		<h2> My Age is : {age} </h2>
 		<h3> I belongs to the Indian state of  "{state}"</h3>
 		<h4> I did my graduation in : {year} </h4>
-		{/* <p> My Sequence is : {numCount}</p> */}
-		{/* <p> My Object is : {objTwo}</p>  */}
-		{/* <p> My Object is : {objTwo.a}</p>  */} 
+		<p> My Sequence is : {numCount}</p>
+		<p> My Object is : {objTwo}</p> 
+		<p> My Object is : {objTwo.a}</p>  
 		<p>{list}</p>
 		</div>
 	);
--- 01_React_file/src/ProductGroup.jsx
+++ 01_React_file/src/ProductGroup.jsx
@@ -3,16 +3,16 @@ import "./ProductGroup.css"
 import Product from "./Product.jsx";
 
 function ProductGroup (){
-	// const nums = ['Pratham', 'Dwitya', 'Tritya', 'Chaturth']
+	const nums = ['Pratham', 'Dwitya', 'Tritya', 'Chaturth']
 	let num2 =  { pehla : 'Pratham', dudra : 'Dwitya', teesra : 'Tritya', chautha: 'Chaturtha' }
 	let options = ["Hi-tech", "durable", "fast"];
 
 	return (
 		<div className="div2">
-		<Product name="Ramesh" age= {22}   state="Rajasthan" /* sequence = {nums}  objTwo = {['Hello', 'World']} */  features ={options} />
+		<Product name="Ramesh" age= {22}   state="Rajasthan" sequence = {nums}  objTwo = {['Hello', 'World']}  features ={options} />
 		{/* <Product name="Ramesh" age= {22}   state="Rajasthan"  sequence = {num}  objTwo = { {'a': 'Hello', 'b': 'World'}} /> */}
-		{/* <Product name="Vimal" age= {44}    state="Haryana"  sequence = {nums}  /> */}
-		{/* <Product name="Suresh" age = {55/2}  state="Tamil Nadu"  sequence = {nums}  /> */}
+		{/* <Product name="Vimal" age= {44}    state="Haryana"  sequence = {nums}   features ={options}  /> */}
+		{/* <Product name="Suresh" age = {55/2}  state="Tamil Nadu"  sequence = {nums}   features ={options}  /> */}
 		</div>
 	)
 }
