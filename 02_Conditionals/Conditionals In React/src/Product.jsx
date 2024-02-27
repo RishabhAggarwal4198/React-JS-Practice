766b2fea7fa6e04350363526d55d88e0d9248679
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 15:37:22 +0530

    Changes

---
+++ 02_Conditionals/Conditionals In React/src/Product.jsx
@@ -0,0 +1,23 @@
+
+import "./Product.css"
+
+
+function Product ({name, age, state, year = `"Not mentioned"`}) {
+
+
+
+	return (
+		<div className="div1">
+		<h1> Hello My Name is {name} </h1>
+		<h2> My Age is : {age} </h2>
+		<h3> I belongs to the Indian state of  "{state}"</h3>
+		<h4> I did my graduation in year : {year} </h4>
+		</div>
+	);
+}
+
+
+
+
+
+export default Product
---
+++ 02_Conditionals/Conditionals In React/src/ProductGroup.jsx
@@ -0,0 +1,17 @@
+
+import "./ProductGroup.css"
+import Product from "./Product.jsx";
+
+function ProductGroup (){
+	
+
+	return (
+		<div className="div2">
+		<Product name="Ramesh" age= {22}   state="Rajasthan" year= {2002} />
+		<Product name="Vimal" age= {44}    state="Haryana"   year= {2005}  />
+		<Product name="Suresh" age = {55/2}  state="Tamil Nadu"  year= {2011}   />
+		</div>
+	)
+}
+
+export default ProductGroup 
