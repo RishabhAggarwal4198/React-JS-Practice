00cef272e565e0688617c2323cd11a891e87e2a8
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-02-27 23:27:06 +0530

    Changes

---
+++ 04_Product_Cards/Product Cards/src/Card.jsx
@@ -0,0 +1,18 @@
+
+
+function Card ({heading, image, dualDescrptn , price}) {
+	
+	return (
+		<>
+		<h1>{heading}</h1>
+		<img src= {image} alt="" />
+		<ul>
+			<li> </li>
+			<li> </li>
+		</ul>
+		
+		<h2>{price}</h2>
+		</>
+	)
+	
+}
